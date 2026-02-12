c0764d2bc12afae5e8549ff76f05cd03c12cfd97
Bryceells <bryceells@live.com>
2026-02-12 14:39:36 -0800

    Added .md file with answers to questions.

--- LuckySpin/ViewModels/PlayersChoice.cs
+++ LuckySpin/ViewModels/PlayersChoice.cs
@@ -5,6 +5,9 @@ namespace LuckySpin.ViewModels
     {
         public Player Player { get; set; }
         //TODO: Add  additional properties needed to pre-fill the PlayersChoice View.
+        public int GameId { get; set; }
+        public int SpinId { get; set; }
+        public int Luck { get; set; }
 
     }
 }
